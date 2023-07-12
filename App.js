30d43af9734f1679208678e27298b7876baee612
IdrisFallout <IdrisFallout>
2023-07-12 13:55:28 +0000

    Pushed from Locofy Builder

--- App.js
+++ App.js
@@ -3,21 +3,58 @@ import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { useFonts } from "expo-font";
 import Splashscreen from "./screens/Splashscreen";
+import LoginScreen from "./screens/LoginScreen";
+import MIcon from "react-native-vector-icons/MaterialCommunityIcons";
+import { IconRegistry, ApplicationProvider } from "@ui-kitten/components";
+import * as eva from "@eva-design/eva";
 
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { View, Text, Pressable, TouchableOpacity } from "react-native";
+import {
+  View,
+  Text,
+  Pressable,
+  TouchableOpacity,
+  StyleSheet,
+} from "react-native";
 
 const App = () => {
   const [hideSplashScreen, setHideSplashScreen] = React.useState(false);
   const [fontsLoaded, error] = useFonts({
+    Inter_extrabold: require("./assets/fonts/Inter_extrabold.ttf"),
     Inter_extrabold_italic: require("./assets/fonts/Inter_extrabold_italic.ttf"),
   });
 
   React.useEffect(() => {
     setTimeout(() => {
       setHideSplashScreen(true);
-    }, 2000);
+    }, 1000);
   }, []);
+
+  function MaterialIcon({ name, style }) {
+    const { height, tintColor, ...iconStyle } = StyleSheet.flatten(style);
+    return (
+      <MIcon name={name} size={height} color={tintColor} style={iconStyle} />
+    );
+  }
+
+  const IconProvider = (name) => ({
+    toReactElement: (props) => MaterialIcon({ name, ...props }),
+  });
+
+  function createIconsMap() {
+    return new Proxy(
+      {},
+      {
+        get(target, name) {
+          return IconProvider(name);
+        },
+      }
+    );
+  }
+  const MaterialIconsPack = {
+    name: "material",
+    icons: createIconsMap(),
+  };
 
   if (!fontsLoaded && !error) {
     return null;
@@ -25,19 +62,30 @@ const App = () => {
 
   return (
     <>
-      <NavigationContainer>
-        {hideSplashScreen ? (
-          <Stack.Navigator screenOptions={{ headerShown: false }}>
-            <Stack.Screen
-              name="Splashscreen"
-              component={Splashscreen}
-              options={{ headerShown: false }}
-            />
-          </Stack.Navigator>
-        ) : (
-          <Splashscreen />
-        )}
-      </NavigationContainer>
+      <IconRegistry icons={[MaterialIconsPack]} />
+      <ApplicationProvider {...eva} theme={eva.light}>
+        <NavigationContainer>
+          {hideSplashScreen ? (
+            <Stack.Navigator
+              initialRouteName="LoginScreen"
+              screenOptions={{ headerShown: false }}
+            >
+              <Stack.Screen
+                name="Splashscreen"
+                component={Splashscreen}
+                options={{ headerShown: false }}
+              />
+              <Stack.Screen
+                name="LoginScreen"
+                component={LoginScreen}
+                options={{ headerShown: false }}
+              />
+            </Stack.Navigator>
+          ) : (
+            <Splashscreen />
+          )}
+        </NavigationContainer>
+      </ApplicationProvider>
     </>
   );
 };
