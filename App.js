d236687db631542f1ff0b029bc2a70b3eb448354
IdrisFallout <IdrisFallout>
2023-07-12 10:54:17 +0000

    First Commit from Locofy Builder

---
+++ App.js
@@ -0,0 +1,37 @@
+const Stack = createNativeStackNavigator();
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+
+import Splashscreen from "./screens/Splashscreen";
+
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { View, Text, Pressable, TouchableOpacity } from "react-native";
+
+const App = () => {
+  const [hideSplashScreen, setHideSplashScreen] = React.useState(false);
+
+  React.useEffect(() => {
+    setTimeout(() => {
+      setHideSplashScreen(true);
+    }, 2000);
+  }, []);
+
+  return (
+    <>
+      <NavigationContainer>
+        {hideSplashScreen ? (
+          <Stack.Navigator screenOptions={{ headerShown: false }}>
+            <Stack.Screen
+              name="Splashscreen"
+              component={Splashscreen}
+              options={{ headerShown: false }}
+            />
+          </Stack.Navigator>
+        ) : (
+          <Splashscreen />
+        )}
+      </NavigationContainer>
+    </>
+  );
+};
+export default App;
---
+++ screens/Splashscreen.js
@@ -0,0 +1,50 @@
+import * as React from "react";
+import { Image, StyleSheet, View } from "react-native";
+
+const Splashscreen = () => {
+  return (
+    <View style={[styles.splashscreen, styles.splashscreenFlexBox]}>
+      <View style={[styles.logoContainer, styles.splashscreenFlexBox]}>
+        <Image
+          style={styles.logoIcon}
+          resizeMode="cover"
+          source={require("../assets/logo.png")}
+        />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  splashscreenFlexBox: {
+    justifyContent: "center",
+    alignItems: "center",
+    overflow: "hidden",
+  },
+  logoIcon: {
+    width: 91,
+    height: 91,
+  },
+  logoContainer: {
+    borderRadius: 10,
+    backgroundColor: "#224092",
+    shadowColor: "rgba(0, 0, 0, 0.49)",
+    shadowOffset: {
+      width: 4.900000095367432,
+      height: 3.5,
+    },
+    shadowRadius: 4.9,
+    elevation: 4.9,
+    shadowOpacity: 1,
+    width: 109,
+    height: 109,
+  },
+  splashscreen: {
+    backgroundColor: "#fff",
+    flex: 1,
+    width: "100%",
+    height: 800,
+  },
+});
+
+export default Splashscreen;
